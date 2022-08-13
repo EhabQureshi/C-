796189f13d6ef04e7de5f88705051a4314fef5d5
EhabQureshi <106132798+EhabQureshi@users.noreply.github.com>
2022-08-13 18:44:19 +0530

    Create division.c

---
+++ division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,div;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("enter the value of b\n");
+    scanf("%d",&b);
+    div=a/b;
+    printf("the division is %f",div);
+    
+    return 0;
+}
+
