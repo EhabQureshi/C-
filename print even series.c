154dfc8332fd15c4bf5e0779b7a8f8fafbcd1da6
EhabQureshi <106132798+EhabQureshi@users.noreply.github.com>
2022-08-13 23:04:30 +0530

    Create print even series.c

---
+++ print even series.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("enter the value ");
+    scanf(" %d ",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i*2);
+        
+    }
+    return 0;
+}
