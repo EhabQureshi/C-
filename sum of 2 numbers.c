c7bf0bd8a9cf483a4f9e5469a341e996025e7938
EhabQureshi <106132798+EhabQureshi@users.noreply.github.com>
2022-08-13 18:31:50 +0530

    Create sum of 2 numbers.c

---
+++ sum of 2 numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the value of a\n");
+    scanf("%d",&a);
+    printf("enter the value of b\n");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("%d+%d=%d",a,b,sum);
+    
+    return 0;
+}
