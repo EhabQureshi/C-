faf26ccb8f1807d6bfadfa8fad138cdf54b78e8d
EhabQureshi <106132798+EhabQureshi@users.noreply.github.com>
2022-08-14 12:18:45 +0530

    Create odd even.c

---
+++ odd even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    printf("enter the value\n");
+    scanf("%d",&a);
+    if(a % 2==0)
+    printf("the no is even");
+    else
+    printf("the number is odd");
+
+    
+    
+
+    return 0;
+}
