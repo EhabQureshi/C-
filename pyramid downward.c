1c0e6b62c0f3f2940ead6aa1ad16d2e1f23c6f54
EhabQureshi <106132798+EhabQureshi@users.noreply.github.com>
2022-09-21 16:39:27 +0530

    Create pyramid downward.c

---
+++ pyramid downward.c
@@ -0,0 +1,28 @@
+/* print following pattern:
+*	*	*	*	*	*	
+*	*	*	*	*	
+*	*	*	*	
+*	*	*	
+*	*	
+*	
+	
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for(i=1;i<=6;i++)
+    {
+        for(j=6;j>=i;j--)
+        { 
+            printf("*\t");
+        }
+         printf("\n");   
+    }
+    
+    
+    return 0;
+}
+
+
